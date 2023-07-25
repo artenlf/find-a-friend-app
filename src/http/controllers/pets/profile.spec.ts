bebeab71ef95ace4aa46aa0a302d632e8e3189d9
artenlf <felipe.arten@gmail.com>
2023-07-25 00:06:15 -0300

    test: add profile http

---
+++ src/http/controllers/pets/profile.spec.ts
@@ -0,0 +1,48 @@
+import { app } from '@/app'
+import { prisma } from '@/lib/prisma'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Profile Pets (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to find a Pet by id and get its profile', async () => {
+    const organization = await prisma.organization.create({
+      data: {
+        title: 'John Doe Organization',
+        email: 'johndoe@example.com',
+        zip_code: '12345678',
+        address: '39, 5th street',
+        phone: '12345678',
+        password_hash: '12345678',
+        latitude: -22.9019746,
+        longitude: -47.0582353,
+      },
+    })
+
+    const pet = await prisma.pet.create({
+      data: {
+        type: 'dog',
+        name: 'bidu',
+        about: 'dog legal',
+        age: 'young',
+        size: 'small',
+        energy_level: 'medium',
+        independency_level: 'medium',
+        environment: 'medium',
+        organization_id: organization.id,
+      },
+    })
+
+    const response = await request(app.server).get(`/pets/${pet.id}`).send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.pet.name).toEqual('bidu')
+  })
+})
