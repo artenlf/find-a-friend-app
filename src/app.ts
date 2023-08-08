0b7383a7836bb8ef37d8f70dd030197a1d66b02d
arten <felipe.arten@gmail.com>
2023-08-08 17:13:41 -0300

    feat/tests: create refresh token and tests

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
+import fastifyCookie from '@fastify/cookie'
 import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 import { env } from './env'
 import { organizationsRoutes } from './http/controllers/organizations/routes'
 import { petsRoutes } from './http/controllers/pets/routes'
@@ -8,26 +10,32 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
   sign: {
     expiresIn: '10m',
   },
 })
 
+app.register(fastifyCookie)
+
 app.register(petsRoutes)
 app.register(organizationsRoutes)
 
-// app.setErrorHandler((error, _request, reply) => {
-//   if (error instanceof ZodError) {
-//     return reply
-//       .status(400)
-//       .send({ message: 'Validation error', issues: error.format() })
-//   }
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error', issues: error.format() })
+  }
 
-//   if (env.NODE_ENV !== 'production') {
-//     console.error(error)
-//   } else {
-//     // TODO: Here we should log the error to an external tool like Datadog/NewRelic/Sentry
-//   }
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log the error to an external tool like Datadog/NewRelic/Sentry
+  }
 
-//   return reply.status(500).send({ message: 'Internal server error' })
-// })
+  return reply.status(500).send({ message: 'Internal server error' })
+})
--- src/http/controllers/organizations/routes.ts
+++ src/http/controllers/organizations/routes.ts
@@ -3,10 +3,12 @@ import { FastifyInstance } from 'fastify'
 import { authenticate } from './authenticate'
 import { create } from './create'
 import { profile } from './profile'
+import { refresh } from './refresh-token'
 
 export async function organizationsRoutes(app: FastifyInstance) {
   app.post('/organizations', create)
   app.post('/sessions', authenticate)
+  app.patch('/token/refresh', refresh)
 
   // Authenticated routes
   app.get('/organization', { onRequest: [verifyJWT] }, profile)
